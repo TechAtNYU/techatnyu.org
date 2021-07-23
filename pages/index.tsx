ee0dcdef04d7236e692bd970151e09c1eec3091d
Anthony Ardi Sukotjo <aas10023@nyu.edu>
2021-07-23 23:31:41 +0800

    basic footer skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import React from "react";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -52,18 +54,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer/>
     </div>
   )
 }
