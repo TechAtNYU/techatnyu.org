35812a183f6d1ffcf395d36e2bcf5d55e58f57a9
Jen-Lopez <jennlolopz@gmail.com>
2021-08-06 17:41:05 -0400

    update team page

--- pages/team.tsx
+++ pages/team.tsx
@@ -9,22 +9,22 @@ export default function Team() {
             <h1 className="team-heading">Meet the Team</h1>
             <Row>
                 <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Kelly Mao"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Juliana Regen"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Ozi Chukwukeme"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Andrew Huang"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Karina Zhang"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Lucas Ortiz"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Gaurav Kulkarni"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Jennifer Lopez"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Anthony Sukotjo"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
             </Row>
 
             <h1 className="team-heading">Notable Alumni</h1>
             <Row>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
-                <IndivTeamMemberCard imgSrc={"../assets/team-photos/jayli.jpeg"} name={"Jay Li Quek"} role={"President"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Alum"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Alum"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
+                <IndivTeamMemberCard imgSrc={"../assets/team-photos/temp.jpg"} name={"Alum"} role={"Role"} linkedin={"https://www.linkedin.com"} github={"https://www.github.com"} website={"https://www.google.com"}/>
             </Row>
         </div>
     )
