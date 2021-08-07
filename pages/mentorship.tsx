87ac213fe8421af1a0e32b9f4a67a25f402084a8
Jen-Lopez <jennlolopz@gmail.com>
2021-08-07 00:04:38 -0400

    fix mentorship

--- pages/mentorship.tsx
+++ pages/mentorship.tsx
@@ -3,106 +3,60 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Col, Row} from "react-bootstrap";
 import GeneralButton from "../components/GeneralButton";
 
-
-const mentorShipPageStyle = {
-    padding: "0px 12px",
-    paddingLeft: "72px"
-} as React.CSSProperties;
-
-const headerStyle = {
-    fontSize: "84px",
-    fontWeight: 700,
-    marginTop: "154px",
-    marginBottom:"64px",
-    textAlign: "start"
-} as React.CSSProperties;
-
-const descriptionStyle = {
-    fontSize:"18px",
-    marginBottom:"40px",
-    textAlign: "start",
-} as React.CSSProperties;
-
-const h1Style = {
-    fontWeight : 500,
-    fontSize: "30px",
-    marginTop: "60px",
-    marginBottom:"16px",
-
-}
-
-const h2style = {
-    fontWeight : 400,
-    fontSize: "22px",
-    marginBottom:"16px"
-}
-
-const rowTextStyle = {
-    fontWeight : 400,
-    fontSize: "18px",
-}
-
-const mentorshipLogoStyle = {
-    marginBottom: "-277px",
-    marginLeft: "317px",
-}
 export default function Mentorship() {
     return (
-        <div style ={mentorShipPageStyle} >
-            <Col>
-                <img src={"../assets/headings/wateringplant.svg"} alt={"mentorship logo"} style={mentorshipLogoStyle}/>
-              <div style={headerStyle}>
-                  Mentorship
-              </div>
-                <div style ={descriptionStyle}>
-                    <div>
-                        an informal chat with tech@nyu alumni and seniors at NYU to receive
-                    </div>
-                    <div>
-                        guidance about applying for a job, prepping for recruiting, finding out
-                    </div>
-                    <div>
-                        what start up world is like and the more.
-                    </div>
-                </div>
+        <div className="program">
+            <div className="container program__header">
+                <img src = "../assets/headings/mentorship.png"  style={{padding: "5% 5% 5% 0"}}/>
+                <p style={{marginBottom:"30px"}}>
+                    an informal chat with tech@nyu alumni and seniors at NYU to receive guidance about applying for a job, 
+                    prepping for recruting, finding out what start up world is like and the more.
+                </p>
                 <GeneralButton name={"Access the database"}/>
-                <Row style={rowTextStyle}>
-                    <Col md>
-                      <h1 style ={h1Style}>  Are you a...</h1>
-                        <h2 style={h2style}>
-                            Student
-                        </h2>
-                        <ul style={{   marginBottom:"72px"} } >
-                            <li style={{fontSize:"18px"}}> looking for more personalized career advice? </li>
-                            <li> curious about entrepreneurship? </li>
-                            <li> trying to figure out what career paths there are in tech?</li>
-                        </ul>
-                        <h2  style={h2style}>
-                            Tech@NYU Alumni / Junior / Senior
-                        </h2>
-                        <ul style={{ marginBottom:"171px"}}>
-                            <li>willing to share about your experiences in Tech?</li>
-                            <li>interested in chatting with curious students?</li>
-                            <li>wanting to pass down your coding/design/product management skills?</li>
-                        </ul>
-
+            </div>
+            <div className="container program__info" style={{paddingBottom:"10%"}}>
+                <Row>
+                    <Col className="mentorship">
+                        <h2>Are you a...</h2>
+                        <div>
+                            <p className="mentorship__subheading">Student</p>
+                            <ul>
+                                <li>looking for more personalized career advice?</li>
+                                <li>curious about entrepreneurship?</li>
+                                <li>trying to figure out what career paths there are in tech?</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p className="mentorship__subheading">Tech@NYU Alumni / Junior / Senior</p>
+                            <ul>
+                                <li>willing to share about your experiences in Tech?</li>
+                                <li>interested in chatting with curious students?</li>
+                                <li>wanting to pass down your coding/design/product management skills?</li>
+                            </ul>
+                        </div>
                     </Col>
-                    <Col md>
-                       <h1 style ={h1Style}> What you'll get out of Mentorship</h1>
-                        <ul style={{marginTop: "45px",marginBottom:"100px"}}>
-                            <li>learn some skills hosted by different mentors                 </li>
-                            <li>potentially get to work on a project with your mentor        </li>
-                            <li>gain a better understanding of a desired/potential role in tech </li>
-                        </ul>
-                        <ul>
-                            <li>meet other mentors & catch up with Tech@NYU alumni  </li>
-                            <li>a peep of managing juniors              </li>
-                            <li>meet someone new!                 </li>
-                            <li>play a part in cultivating budding talent in tech</li>
-                        </ul>
+                    <Col className="mentorship">
+                        <h2>What you'll get out of Mentorship</h2>
+                        <div>
+                            <p className="mentorship__subheading hide">Student</p>
+                            <ul>
+                                <li>learn some skills hosted by different mentors</li>
+                                <li>potentially get to work on a project with your mentor</li>
+                                <li>gain a better understanding of a desired/potential role in tech</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p className="mentorship__subheading hide">Tech@NYU Alumni / Junior / Senior</p>
+                            <ul>
+                                <li>meet other mentors &amp; catch up with Tech@NYU alumni</li>
+                                <li>a peep of managing juniors</li>
+                                <li>meet someone new!</li>
+                                <li>play a part in cultivating budding talent in tech</li>
+                            </ul>
+                        </div>
                     </Col>
                 </Row>
-            </Col>
+            </div>
         </div>
     )
 }
