8df19eacb43b597f47e168910a85c0d25e4f8b2c
Zachary <zw1490@nyu.edu>
2021-10-03 23:00:59 -0400

    wassup

---
+++ components/HowItWorksBox.tsx
@@ -0,0 +1,8 @@
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"> My first Box  </h5>
+    <p class="card-text">Hey everybody is this a box?</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
