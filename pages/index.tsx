76fa01d473e93f8de8b51e861e88c7f97a302dd4
Indeera Saha <58609618+indeerasaha@users.noreply.github.com>
2025-02-04 14:15:36 -0500

    adding applications are open to website for spring 2025

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,12 @@ export default function Home() {
   return (
     <div>
 
+      <div className="container" style={{ backgroundColor: "white", padding: "15px", marginTop: "20px", borderRadius: "5px", boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)" }}>
+        <p style={{ textAlign: "center", margin: "0", fontSize: "18px", fontWeight: "bold" }}>
+          Applications for Spring 2025 Tech Treks, Mentorship, and Dev Team cohorts are open now! Apps close 11:59PM February 10th, 2025!
+        </p>
+      </div>
+
         <div id="header" className="container">
           <div className="tagline">
             <p>Tech@NYU is a space for artists, makers and hackers to build stuff together, learn new skills, and explore opportunities in tech. </p>
